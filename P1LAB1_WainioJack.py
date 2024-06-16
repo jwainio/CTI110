7220b0bea2481c34444a121f1f34371da70a4437
jwainio <wainioj5132@student.faytechcc.edu>
2024-06-16 12:29:47 -0400

    Add files via upload

---
+++ P1LAB1_WainioJack.py
@@ -0,0 +1,11 @@
+# Jack Wainio
+# June 14th, 2024
+# P1LAB1
+# This is a welcome message to users
+
+first_name = input("Please enter your first name: ")
+
+last_name = input("Please enter your last name: ")
+
+print(f"Welcome, {first_name} {last_name}, to this course!")
+
